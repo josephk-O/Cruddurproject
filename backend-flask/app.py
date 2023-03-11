a2531f9d1a3fbc0a3280594458a3a67abe0e213b
ogunmolak <ogunmolakay@outlook.com>
2023-03-11 18:39:11 +0000

    wk 3 fix

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -166,16 +166,17 @@ def data_home():
   access_token = extract_access_token(request.headers)
   try:
     claims = cognito_jwt_token.verify(access_token)
-    #authenticated request
-    app.logger.debug("authenticated")
+    # authenicatied request
+    app.logger.debug("authenicated")
     app.logger.debug(claims)
     app.logger.debug(claims['username'])
     data = HomeActivities.run(cognito_user_id=claims['username'])
   except TokenVerifyError as e:
-    #unauthenticated request
+    # unauthenicatied request
     app.logger.debug(e)
-    app.logger.debug("unauthenticated")
+    app.logger.debug("unauthenicated")
     data = HomeActivities.run()
+    
   return data, 200
 
 @app.route("/api/activities/notifications", methods=['GET'])
