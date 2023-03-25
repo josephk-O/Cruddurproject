7c7d991d3fae43142eed94d636afc8e39b6e5d3f
ogunmolak <ogunmolakay@outlook.com>
2023-03-25 19:22:03 +0000

    broken ---- too be fixed

--- backend-flask/db/seed.sql
+++ backend-flask/db/seed.sql
@@ -1,14 +1,13 @@
 -- this file was manually created
 INSERT INTO public.users (display_name, email, handle, cognito_user_id)
 VALUES
-  ('Andrew Brown','andrew@exampro.co' , 'andrewbrown' ,'MOCK'),
-  ('Andrew Bayko','bayko@exampro.co' , 'bayko' ,'MOCK');
-  ('Londo Mollari','lmollari@centari.com' ,'londo' ,'MOCK');
+  ('josephkay','ogunmolakay@outlook.com' , 'josephkay' ,'2641a6f0-e09e-4389-92c9-f05668b7c665'),
+  ('Andrew Bayko','josephcrypto98@gmail.com' , 'bayko' ,'2e26d104-50b9-4fa3-bb09-708abe9fd04a');
 
 INSERT INTO public.activities (user_uuid, message, expires_at)
 VALUES
   (
-    (SELECT uuid from public.users WHERE users.handle = 'andrewbrown' LIMIT 1),
+    (SELECT uuid from public.users WHERE users.handle = 'josephkay' LIMIT 1),
     'This was imported as seed data!',
     current_timestamp + interval '10 day'
   )
