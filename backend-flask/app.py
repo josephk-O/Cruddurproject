33188f194614c76830d0426b0e4b250a88552c86
ogunmolak <86959956+ogunmolak@users.noreply.github.com>
2023-04-10 11:52:23 +0000

    appy.py rebuild

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -150,17 +150,19 @@ def data_message_groups():
 
 @app.route("/api/messages/<string:message_group_uuid>", methods=['GET'])
 def data_messages(message_group_uuid):
+#user_sender_handle = 'josephkay'
+#user_receiver_handle = request.args.get('user_reciever_handle')
+
   access_token = extract_access_token(request.headers)
   try:
     claims = cognito_jwt_token.verify(access_token)
-    # authenicatied request
     app.logger.debug("authenicated")
     app.logger.debug(claims)
     cognito_user_id = claims['sub']
     model = Messages.run(
-        cognito_user_id=cognito_user_id,
-        message_group_uuid=message_group_uuid
-      )
+      cognito_user_id=cognito_user_id,
+      message_group_uuid=message_group_uuid
+    )
     if model['errors'] is not None:
       return model['errors'], 422
     else:
@@ -173,9 +175,9 @@ def data_messages(message_group_uuid):
 @app.route("/api/messages", methods=['POST','OPTIONS'])
 @cross_origin()
 def data_create_message():
-  message_group_uuid   = request.json.get('message_group_uuid',None)
-  user_receiver_handle = request.json.get('handle',None)
-  message = request.json['message']
+  #message_group_uuid   = request.json.get('message_group_uuid',None)
+  #user_receiver_handle = request.json.get('handle',None)
+  #message = request.json['message']
   access_token = extract_access_token(request.headers)
   try:
     claims = cognito_jwt_token.verify(access_token)
@@ -234,6 +236,11 @@ def data_notifications():
   data = NotificationsActivities.run()
   return data, 200
 
+@app.route("/api/users/@<string:handle>/short", methods=['GET'])
+def data_users_short(handle):
+  data = UsersShort.run(handle)
+  return data, 200
+
 @app.route("/api/activities/@<string:handle>", methods=['GET'])
 #@xray_recorder.capture('activities_users')
 def data_handle(handle):
@@ -284,10 +291,6 @@ def data_activities_reply(activity_uuid):
     return model['data'], 200
   return
 
-@app.route("/api/users/@<string:handle>/short", methods=['GET'])
-def data_users_short(handle):
-  data = UsersShort.run(handle)
-  return data, 200
 
 if __name__ == "__main__":
   app.run(debug=True)
