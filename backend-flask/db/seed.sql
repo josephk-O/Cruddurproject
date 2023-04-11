4744c4755be512e353e9ee853a9feb40a993b212
ogunmolak <ogunmolakay@outlook.com>
2023-04-11 14:09:29 +0000

    done dynamo db

--- backend-flask/db/seed.sql
+++ backend-flask/db/seed.sql
@@ -2,8 +2,7 @@
 INSERT INTO public.users (display_name, email, handle, cognito_user_id)
 VALUES
   ('josephkay','ogunmolakay@outlook.com' , 'josephkay' ,'2641a6f0-e09e-4389-92c9-f05668b7c665'),
-  ('Andrew Bayko','josephcrypto98@gmail.com' , 'bayko' ,'2e26d104-50b9-4fa3-bb09-708abe9fd04a');
-
+  ('Andrew Bayko','josephcrypto98@gmail.com' , 'bayko' ,'2e26d104-50b9-4fa3-bb09-708abe9fd04a'),
   ('Londo Mollari','lmollari@centari.com' , 'londo' ,'MOCK');
 
 
